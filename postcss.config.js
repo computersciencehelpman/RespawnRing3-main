84814a2aa52584cacc819f39b4a9da0c6d27e80f
Saurav Bhatt <55709609+computersciencehelpman@users.noreply.github.com>
2026-01-19 11:52:11 -0500

    Need to upload node modules

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // ← required for Tailwind 4+
+    autoprefixer: {},
+  },
+};
---
+++ postcss.config.mjs
@@ -0,0 +1,7 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // ✅ correct for Tailwind v4
+    autoprefixer: {},
+  },
+};
